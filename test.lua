3eff19b33b266030f23a1fc81d43febf019c17a1
M7md123 <107771107+M7md123@users.noreply.github.com>
2025-05-05 01:10:35 +0300

    update test.lua

--- test.lua
+++ test.lua
@@ -1,5 +1,16 @@
--- testing
+local ScreenGui = Instance.new("ScreenGui", game:GetService("CoreGui"))
+ScreenGui.Name = "MohaESP_UI"
 
-local ScreenGui = Instance.new("ScreenGui", game:GetService("CoreGui")) ScreenGui.Name = "MohaESP_UI"
-
-local Label = Instance.new("TextLabel") Label.Parent = ScreenGui Label.Text = "Moha the Legend" Label.Size = UDim2.new(0, 300, 0, 50) Label.Position = UDim2.new(0, -300, 1, -100) Label.BackgroundTransparency = 1 Label.TextColor3 = Color3.fromRGB(255, 255, 255) Label.TextStrokeTransparency = 0 -- يعطي Outline Label.TextStrokeColor3 = Color3.fromRGB(0, 0, 0) Label.Font = Enum.Font.Garamond Label.TextSize = 24 Label.TextXAlignment = Enum.TextXAlignment.Center Label.TextYAlignment = Enum.TextYAlignment.Center
+local Label = Instance.new("TextLabel")
+Label.Parent = ScreenGui
+Label.Text = "Moha the Legend"
+Label.Size = UDim2.new(0, 300, 0, 50)
+Label.Position = UDim2.new(0, 100, 0, 100)
+Label.BackgroundTransparency = 1
+Label.TextColor3 = Color3.fromRGB(255, 255, 255)
+Label.TextStrokeTransparency = 0 -- يعطي Outline
+Label.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
+Label.Font = Enum.Font.GothamBold
+Label.TextSize = 24
+Label.TextXAlignment = Enum.TextXAlignment.Center
+Label.TextYAlignment = Enum.TextYAlignment.Center

3aed3acba79af67472b4c5094c5ed9f122971f88
M7md123 <107771107+M7md123@users.noreply.github.com>
2025-05-05 01:28:47 +0300

    update test.lua

--- test.lua
+++ test.lua
@@ -1,16 +1,2 @@
-local ScreenGui = Instance.new("ScreenGui", game:GetService("CoreGui"))
-ScreenGui.Name = "MohaESP_UI"
-
-local Label = Instance.new("TextLabel")
-Label.Parent = ScreenGui
-Label.Text = "Time"
-Label.Size = UDim2.new(0, 300, 0, 50)
-Label.Position = UDim2.new(0, 0.5, 0, 20)
-Label.BackgroundTransparency = 1
-Label.TextColor3 = Color3.fromRGB(255, 255, 255)
-Label.TextStrokeTransparency = 0 -- يعطي Outline
-Label.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
-Label.Font = Enum.Font.Garamond
-Label.TextSize = 24
 Label.TextXAlignment = Enum.TextXAlignment.Center
-Label.TextYAlignment = Enum.TextYAlignment.Center
+Label.TextYAlignment = Enum.TextYAlignment.Cent
